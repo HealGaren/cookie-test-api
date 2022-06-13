f4f997dac7d4254768231256c088006bad60dd63
최예찬 <cyc1007@midasin.com>
2022-06-13 19:20:02 +0900

    쿠키 발급 테스트

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET home page. */
+router.get('/api/environments', function(req, res, next) {
+
+  const domain = req.query.domain;
+
+  res.cookie('test-cookie', 'monster', {
+    secure: true,
+    httpOnly: true,
+    domain
+  });
+  res.json({
+    message: 'ok'
+  });
+});
+
+
 module.exports = router;
